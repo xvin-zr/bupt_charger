b9df5cb7235418243ee63b76789b678ca6a3d9e3
xvin <xvin@bupt.edu.cn>
2023-04-27 22:14:41 +0800

    add wait zone js and json

---
+++ bupt_charger_back/class/wait-zone.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+
+class WaitZone {
+    constructor() {
+        this.waitZone = [];
+        this.filePath = 'json/wait-zone.json';
+        this.loadWaitZone();
+        console.log(this.waitZone);
+    }
+
+    loadWaitZone() {
+        try {
+            const data = fs.readFileSync(this.filePath);
+            this.waitZone = JSON.parse(data);
+        } catch (err) {
+            console.error(`Error loading wait zone: ${err}`);
+        }
+    }
+
+    addUserRequest(chargingMode, chargingAmount, batteryAmount) {
+        const prefix = chargingMode;
+        let queueNumber = '';
+        let maxIndex = 0;
+        let emptyQueueIndex = -1;
+
+        // 找到最大的序号和第一个空队列的位置
+        for (let i = 0; i < this.waitZone.length; i++) {
+            const userReq = this.waitZone[i];
+
+            if (userReq.queueNumber === '') {
+                emptyQueueIndex = i;
+                break;
+            } else if (userReq.queueNumber.startsWith(prefix)) {
+                const index = parseInt(userReq.queueNumber.slice(1));
+                // console.log('index:', index);
+                if (isNaN(index)) {
+                    // emptyQueueIndex = i;
+                    break;
+                }
+                if (index > maxIndex) {
+                    maxIndex = index;
+                }
+            }
+        }
+
+        // 如果没有空位，返回 false
+        if (emptyQueueIndex === -1 ) {
+            return false;
+        } else if (emptyQueueIndex !== -1) {
+            maxIndex++;
+            // 如果有空位，则更新空位为用户请求
+            this.waitZone[emptyQueueIndex] = {
+                queueNumber: prefix + maxIndex.toString(),
+                userReq: {
+                    chargingAmount: chargingAmount,
+                    batteryAmount: batteryAmount
+                }
+            }
+        }
+
+
+
+
+        // 将更新后的 waitZone 保存到 JSON 文件中
+        this.saveWaitZone();
+        return true;
+    }
+
+    clearQueueInfo(queueNumber) {
+        for (let i = 0; i < this.waitZone.length; i++) {
+            const userReq = this.waitZone[i];
+
+            if (userReq.queueNumber === queueNumber) {
+                this.waitZone[i].queueNumber = '';
+                this.waitZone[i].userReq.chargingAmount = 0;
+                this.waitZone[i].userReq.batteryAmount = 0;
+                break;
+            }
+        }
+
+        this.saveWaitZone();
+    }
+
+    saveWaitZone() {
+        try {
+            const data = JSON.stringify(this.waitZone, null, 2);
+            fs.writeFileSync(this.filePath, data);
+        } catch (err) {
+            console.error(`Error saving wait zone: ${err}`);
+        }
+    }
+}
+
+module.exports = WaitZone;
--- bupt_charger_back/routes/charging.js
+++ bupt_charger_back/routes/charging.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const WaitZone = require('../class/wait-zone');
+
 router.post('/request', (req, res) => {
+    const { chargingMode, chargingAmount, batteryAmount } = req.body;
+    console.log(chargingMode, chargingAmount, batteryAmount);
     // 记录申请信息
+    const waitZone = new WaitZone();
+    const addRes = waitZone.addUserRequest(chargingMode, chargingAmount, batteryAmount);
 
-    // store success
-    res.status(200).json({
-        code: 0,
-        message: '申请成功',
-        data: {}
-    })
+    if (addRes) {
+        // store success
+        res.status(200).json({
+            code: 0,
+            message: '申请成功',
+            data: {}
+        });
+    } else {
+        // store fail
+        res.status(401).json({
+            code: -1,
+            message: '申请失败，等候区已满',
+            data: {}
+        })
 
-    // store fail
+    }
+
+
+
+
 
 })
 
