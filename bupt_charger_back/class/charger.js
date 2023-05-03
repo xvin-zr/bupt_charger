17ba6713a7c50463efaec3e97a07408b6a57442a
xvin <xvin@bupt.edu.cn>
2023-05-03 14:19:35 +0800

    add finish charging

--- bupt_charger_back/class/charger.js
+++ bupt_charger_back/class/charger.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 const csv = require("csv");
+const crypto = require('crypto');
+const { v4: uuidv4 } = require('uuid');
+
+
 
 // 峰时电费单价
 const HIGH_PRICE = 1.0;
@@ -38,24 +42,29 @@ class Charger {
         for (const charger of this.chargers) {
             if (charger.status === "RUNNING") {
                 const task = charger.chargerQueue[0]; // 获取队列中的第一个任务
-                const remainAmount = task.remainAmount;
+
+                if (!task.username) continue;
+
+                const actualPower = Math.min(charger.power, task.remainAmount); // 取实际用电量和剩余电量的最小值作为充电量
+
+                const chargingFee = price * actualPower; // 根据实际用电量计算充电费用
+                const serviceFee = SERVICE_FEE_PRICE * actualPower;
+                task.chargingFee += chargingFee; // 更新充电费用
+                task.serviceFee += serviceFee;
+                charger.cumulativeChargingAmount += actualPower; // 更新充电桩的累计充电量
+                // charger.cumulativeChargingTime += 60; // 更新充电桩的累计充电时间
+                // 累计充电时间结束后计算
+                charger.cumulativeChargingFee += chargingFee; // 更新充电桩的累计充电费用
+                charger.cumulativeServiceFee += serviceFee; // 更新充电桩的累计服务费用
+                charger.cumulativeFee += chargingFee + serviceFee; // 更新充电桩的累计费用
+
+
                 task.remainAmount -= charger.power; // 消耗电量
-                if (task.remainAmount < 0) {
+                if (task.remainAmount <= 0) {
                     task.remainAmount = 0;
                     this.finishCharging(task.username); // 完成充电任务
-                } else {
-                    const actualPower = Math.min(charger.power, remainAmount); // 取实际用电量和剩余电量的最小值作为充电量
-                    task.chargingAmount += actualPower; // 更新充电量
-                    const chargingFee = price * actualPower; // 根据实际用电量计算充电费用
-                    const serviceFee = SERVICE_FEE_PRICE * actualPower;
-                    task.chargingFee += chargingFee; // 更新充电费用
-                    task.serviceFee += serviceFee;
-                    charger.cumulativeChargingAmount += actualPower; // 更新充电桩的累计充电量
-                    charger.cumulativeChargingTime += 1; // 更新充电桩的累计充电时间
-                    charger.cumulativeChargingFee += chargingFee; // 更新充电桩的累计充电费用
-                    charger.cumulativeServiceFee += serviceFee; // 更新充电桩的累计服务费用
-                    charger.cumulativeFee += chargingFee + serviceFee; // 更新充电桩的累计费用
                 }
+
             }
         }
         this.saveCharger(); // 保存充电桩数据
@@ -64,24 +73,22 @@ class Charger {
     getShutdownChargerUsers() {
         for (const charger of this.chargers) {
             if (charger.status === "SHUTDOWN") {
-                const { chargerType, chargerQueue } = charger;
+                const {chargerType, chargerQueue} = charger;
                 // if (chargerQueue[0].username) {
                 //     this.finishCharging(chargerQueue[0].username);
                 // }
-                return { chargerType: chargerType, user1: chargerQueue[0], user2: chargerQueue[1] };
+                return {chargerType: chargerType, user1: chargerQueue[0], user2: chargerQueue[1]};
             }
         }
         return null;
     }
 
-    append2CSV() {
-        const filePath = "../json/report.csv";
-        const data = {
-
-        };
+    append2CSV(data) {
+        const filePath = "json/report.csv";
+        // const data = {};
 
         // 将新数据追加到CSV文件中
-        csv.stringify([data], { header: false }, (err, output) => {
+        csv.stringify([data], {header: false}, (err, output) => {
             if (err) throw err;
 
             // 使用追加模式打开文件以添加数据
@@ -123,13 +130,78 @@ class Charger {
         // console.log(`No charging task found for user ${username}`);
     }
 
-    finishCharging(username) {
 
+
+    // if userId is a number
+    // const objectHash = require('node-object-hash')({sort: false});
+    //
+    // const surname = 'Smith';
+    // const surnameNumber = objectHash.hash(surname);
+    // console.log(surnameNumber);
+    finishCharging(username) {
+        for (const c of this.chargers) {
+            const queue = c.chargerQueue;
+            if (queue[0].username === username) {
+                // 找到了需要完成充电的用户
+                const dateObj = new Date();
+                const startTime = queue[0].startTime;
+                const endTime = dateObj.toISOString();
+                const start = new Date(startTime);
+                const end = new Date(endTime);
+                const chargingTime = (end.getTime() - start.getTime()) / 1000;
+
+                const hash = crypto.createHash('sha256').update(username).digest('hex'); // 将用户名哈希为固定长度的字符串
+                const userId = uuidv4({ namespace: hash }); // 使用哈希值作为命名空间生成 UUID
+                console.log("userId: " + userId);
+                const data = {
+                    userId: userId,
+                    orderId: queue[0].username + Math.floor(Date.now() / 10000).toString(),
+                    createTime: queue[0].startTime,
+                    chargingPileId: c.chargingPileId,
+                    volume: queue[0].chargingAmount - queue[0].remainAmount,
+                    chargingTime: Math.floor(chargingTime),
+                    startTime: startTime,
+                    endTime: endTime,
+                    chargingFee: queue[0].chargingFee,
+                    serviceFee: queue[0].serviceFee,
+                    totalFee: queue[0].chargingFee + queue[0].serviceFee,
+                    time: Date.now().toString(),
+                };
+                this.append2CSV(data); // 调用append2CSV
+                if (queue[1].username) {
+                    // 如果queue[1]有用户，将其移到queue[0]
+                    queue[0] = queue[1];
+                    queue[0].startTime = new Date().toISOString();
+                    queue[1] = {
+                        username: "",
+                        chargingAmount: 0,
+                        remainAmount: 0,
+                        batteryAmount: 0,
+                        chargingFee: 0,
+                        serviceFee: 0,
+                        startTime: "",
+                    };
+                } else {
+                    // 否则将queue[0]的数据置0
+                    queue[0] = {
+                        username: "",
+                        chargingAmount: 0,
+                        remainAmount: 0,
+                        batteryAmount: 0,
+                        chargingFee: 0,
+                        serviceFee: 0,
+                        startTime: "",
+                    };
+                }
+                break; // 找到了用户，退出循环
+            }
+        }
+        this.saveCharger();
     }
 
     assignUser(chargingType, userReq) {
         const chargers = this.chargers.filter(c => c.chargerType === chargingType &&
-                                                c.status === "RUNNING");
+            c.status === "RUNNING");
 
         // 查找第一个空闲位置
         const firstEmptySlot = chargers
@@ -225,7 +297,7 @@ class Charger {
     hasSlotForFastCharger() {
         // 遍历所有快充充电桩
         for (const charger of this.chargers.filter(c => c.chargerType === "F" &&
-                                                    c.status === "RUNNING")) {
+            c.status === "RUNNING")) {
             // 检查是否有空闲队列位置
             if (charger.chargerQueue.some(q => q.username === "")) {
                 return true;
@@ -237,7 +309,7 @@ class Charger {
     hasSlotForSlowCharger() {
         // 遍历所有慢充充电桩
         for (const charger of this.chargers.filter(c => c.chargerType === "T" &&
-                                                    c.status === "RUNNING")) {
+            c.status === "RUNNING")) {
             // 检查是否有空闲队列位置
             if (charger.chargerQueue.some(q => q.username === "")) {
                 return true;
